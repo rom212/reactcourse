1666058dc7e8eb0dbfab323b98e9323811ec509e
romull <romull@amazon.com>
2020-04-24 20:35:10 -0400

    chaged person

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi I am a React app</h1>
-      <Person name="Roman" age="39"/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi I am a React app</h1>
+        <Person name="Roman0" age="39"> What'sup ?</Person>
+        <Person name="Matt" age="40"> What'sup ?</Person>
+      </div>
+    );
+  }
 }
+
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+const style = {
+    padding: "25px",
+    margin: "20px",
+    border: "2px solid",
+    display: "inline-block"
+}
+
 const person = (props) => {
     return(
-        <div style={{border: "2px solid", display: "inline-block"}}>
+        <div style={style}>
             <p>Hi my name is {props.name}</p>
             <p>I am {props.age}</p>
+            <p>{props.children}</p>
         </div>
     );
 }
